0b7c2ac5759c1475efb55b91b15f30ecedbef897
Julian Fuchs <jfuchs@mit.edu>
2016-01-23 16:34:40 -0500

    Formatting

--- qbnotes.py
+++ qbnotes.py
@@ -17,17 +17,17 @@ import operator
 heroku = os.environ.get('HEROKU') == '1'
 
 if heroku:
-	db_uri = os.environ['DATABASE_URL']
+    db_uri = os.environ['DATABASE_URL']
 else:
-	db_uri = 'sqlite:///db.sqlite3'
-	# db_uri = 'postgres://127.0.0.1:5432/qbnotes'
+    db_uri = 'sqlite:///db.sqlite3'
+    # db_uri = 'postgres://127.0.0.1:5432/qbnotes'
 
 app = Flask(__name__)
 app.config.update(dict(
-	SQLALCHEMY_DATABASE_URI=db_uri,
-	SECRET_KEY='43316b82bca7c9847536d08abaae40a0',
-	PASSWORD_HASH='11de2afa581597d4846ccf4cc6de36e7bc9789a3e044e29baca35f7f',
-	version='0.5.10'
+    SQLALCHEMY_DATABASE_URI=db_uri,
+    SECRET_KEY='43316b82bca7c9847536d08abaae40a0',
+    PASSWORD_HASH='11de2afa581597d4846ccf4cc6de36e7bc9789a3e044e29baca35f7f',
+    version='0.5.10'
 ))
 
 Markdown(app)
@@ -35,46 +35,47 @@ db = SQLAlchemy(app)
 
 
 class Group(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
 
-	def __init__(self, name):
-		self.name = name
+    def __init__(self, name):
+        self.name = name
 
-	def __repr__(self):
-		return '<Group %s>' % self.name
+    def __repr__(self):
+        return '<Group %s>' % self.name
 
 
 class Entry(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String)
-	creator = db.Column(db.String)
-	notes = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    creator = db.Column(db.String)
+    notes = db.Column(db.String)
 
-	date_added = db.Column(db.Date)
+    date_added = db.Column(db.Date)
 
-	group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
-	group = db.relationship('Group', backref=db.backref('entries', lazy='dynamic'))
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+    group = db.relationship(
+        'Group', backref=db.backref('entries', lazy='dynamic'))
 
-	def __init__(self, title, creator, notes, group):
-		self.title = title
-		self.creator = creator
-		self.notes = notes
-		self.group = group
-		self.date_added = date.today()
+    def __init__(self, title, creator, notes, group):
+        self.title = title
+        self.creator = creator
+        self.notes = notes
+        self.group = group
+        self.date_added = date.today()
 
-	def __repr__(self):
-		return '<Entry \'%s\'>' % self.title
+    def __repr__(self):
+        return '<Entry \'%s\'>' % self.title
 
 
 def login_required(f):
-	@wraps(f)
-	def decorated_function(*args, **kwargs):
-		if not session.get('logged_in'):
-			return redirect(url_for('login'))
-		return f(*args, **kwargs)
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
 
-	return decorated_function
+    return decorated_function
 
 
 # main app pages
@@ -82,232 +83,241 @@ def login_required(f):
 @app.route('/')
 @login_required
 def all_groups():
-	return render_template('all_groups.html', groups=Group.query.all())
+    return render_template('all_groups.html', groups=Group.query.all())
 
 
 @app.route('/group/<int:group_id>/')
 @login_required
 def group_detail(group_id):
-	return render_template('group_detail.html', group=Group.query.get_or_404(group_id))
+    return render_template('group_detail.html', group=Group.query.get_or_404(group_id))
 
 
 @app.route('/entry/<int:entry_id>')
 @login_required
 def entry_detail(entry_id):
-	return render_template('entry_detail.html', entry=Entry.query.get_or_404(entry_id))
+    return render_template('entry_detail.html', entry=Entry.query.get_or_404(entry_id))
 
 
 @app.route('/group/new', methods=['POST'])
 @login_required
 def new_group():
-	if not request.form['name']:
-		abort(400)
+    if not request.form['name']:
+        abort(400)
 
-	g = Group(request.form['name'])
-	db.session.add(g)
-	db.session.commit()
+    g = Group(request.form['name'])
+    db.session.add(g)
+    db.session.commit()
 
-	return redirect(url_for('all_groups'))
+    return redirect(url_for('all_groups'))
 
 
 @app.route('/group/<int:group_id>/new', methods=['GET', 'POST'])
 @login_required
 def new_entry(group_id):
-	g = Group.query.get_or_404(group_id)
+    g = Group.query.get_or_404(group_id)
 
-	if request.method == 'POST':
-		if not (request.form['title'] and request.form['creator'] and request.form['notes']):
-			abort(400)
+    if request.method == 'POST':
+        if not (request.form['title'] and request.form['creator'] and request.form['notes']):
+            abort(400)
 
-		e = Entry(request.form['title'], request.form['creator'], request.form['notes'], g)
+        e = Entry(request.form['title'], request.form[
+                  'creator'], request.form['notes'], g)
 
-		db.session.add(e)
-		db.session.commit()
+        db.session.add(e)
+        db.session.commit()
 
-		return redirect(url_for('entry_detail', entry_id=e.id))
+        return redirect(url_for('entry_detail', entry_id=e.id))
 
-	return render_template('new_entry.html', group=g)
+    return render_template('new_entry.html', group=g)
 
 
 @app.route('/entry/<int:entry_id>/edit', methods=['GET', 'POST'])
 @login_required
 def edit_entry(entry_id):
-	e = Entry.query.get_or_404(entry_id)
+    e = Entry.query.get_or_404(entry_id)
 
-	if request.method == 'POST':
-		if not (request.form['title'] and request.form['creator'] and request.form['notes']):
-			abort(400)
+    if request.method == 'POST':
+        if not (request.form['title'] and request.form['creator'] and request.form['notes']):
+            abort(400)
 
-		e.title = request.form['title']
-		e.creator = request.form['creator']
-		e.notes = request.form['notes']
+        e.title = request.form['title']
+        e.creator = request.form['creator']
+        e.notes = request.form['notes']
 
-		db.session.commit()
+        db.session.commit()
 
-		return redirect(url_for('entry_detail', entry_id=e.id))
+        return redirect(url_for('entry_detail', entry_id=e.id))
 
-	return render_template('edit_entry.html', entry=e)
+    return render_template('edit_entry.html', entry=e)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	error = None
-	if request.method == 'POST':
-		if not request.form['password']:
-			abort(400)
+    error = None
+    if request.method == 'POST':
+        if not request.form['password']:
+            abort(400)
 
-		if hashlib.sha224(request.form['password']).hexdigest() == app.config['PASSWORD_HASH']:
-			session['logged_in'] = True
-			return redirect(url_for('all_groups'))
-		else:
-			error = 'Wrong password!'
+        if hashlib.sha224(request.form['password']).hexdigest() == app.config['PASSWORD_HASH']:
+            session['logged_in'] = True
+            return redirect(url_for('all_groups'))
+        else:
+            error = 'Wrong password!'
 
-	return render_template('login.html', error=error)
+    return render_template('login.html', error=error)
 
 
 @app.route('/logout')
 def logout():
-	session['logged_in'] = False
-	return redirect(url_for('login'))
+    session['logged_in'] = False
+    return redirect(url_for('login'))
 
 
 @app.route('/group/<int:group_id>/study/', methods=['GET'])
 @login_required
 def study(group_id):
-	return render_template('study.html', group=Group.query.get_or_404(group_id))
+    return render_template('study.html', group=Group.query.get_or_404(group_id))
 
 
 @app.route('/group/<int:group_id>/study/q')
 @login_required
 def get_questions(group_id):
-	contents = []
+    contents = []
 
-	for e in Entry.query.filter_by(group_id=group_id).order_by(func.random()).limit(10).all():
-		lines = e.notes.split('---')[0].split('\n')
-		for i in xrange(0, 10):
-			line = random.choice(lines).lstrip(' +')  # strip leading spaces & bullets
-			if (len(line) >= 16) or ('**' in line):
-				contents.append({'id': e.id, 'title': e.title, 'creator': e.creator, 'clue': line})
-				break
+    for e in Entry.query.filter_by(group_id=group_id).order_by(func.random()).limit(10).all():
+        lines = e.notes.split('---')[0].split('\n')
+        for i in xrange(0, 10):
+            # strip leading spaces & bullets
+            line = random.choice(lines).lstrip(' +')
+            if (len(line) >= 16) or ('**' in line):
+                contents.append({'id': e.id, 'title': e.title,
+                                 'creator': e.creator, 'clue': line})
+                break
 
-	return json.dumps(contents)
+    return json.dumps(contents)
 
 
 @app.route('/group/<int:group_id>/search')
 @login_required
 def search(group_id):
-	g = Group.query.get_or_404(group_id)
-	query = request.args['q']
-	if not query:
-		return redirect(url_for('group_detail', group_id=group_id))
-	results = Entry.query.filter(Entry.group_id == group_id, Entry.notes.like('%{}%'.format(query)))
-	return render_template('search.html', group=g, results=results, query=query)
+    g = Group.query.get_or_404(group_id)
+    query = request.args['q']
+    if not query:
+        return redirect(url_for('group_detail', group_id=group_id))
+    results = Entry.query.filter(
+        Entry.group_id == group_id, Entry.notes.like('%{}%'.format(query)))
+    return render_template('search.html', group=g, results=results, query=query)
 
 
 @app.route('/group/<int:group_id>/stats')
 @login_required
 def stats(group_id):
-	g = Group.query.get_or_404(group_id)
+    g = Group.query.get_or_404(group_id)
 
-	s = dict()
-	s['nworks'] = g.entries.count()
-	s['creators'] = db.session.query(Entry.creator, label("works", func.count(Entry.id)))\
-		.filter(Entry.group_id == group_id).group_by(Entry.creator).order_by(desc("works")).all()
-	s['ncreators'] = len(s['creators'])
+    s = dict()
+    s['nworks'] = g.entries.count()
+    s['creators'] = db.session.query(Entry.creator, label("works", func.count(Entry.id)))\
+        .filter(Entry.group_id == group_id).group_by(Entry.creator).order_by(desc("works")).all()
+    s['ncreators'] = len(s['creators'])
 
-	nworks_hist = defaultdict(int)
-	for c in s['creators']:
-		nworks_hist[c[1]] += 1
-	s['nworks_hist'] = (nworks_hist, max(nworks_hist.values()))
+    nworks_hist = defaultdict(int)
+    for c in s['creators']:
+        nworks_hist[c[1]] += 1
+    s['nworks_hist'] = (nworks_hist, max(nworks_hist.values()))
 
-	month_hist = dict()
-	start = db.session.query(func.min(Entry.date_added)).filter(Entry.group_id == group_id).one()[0]
-	start = (start.year, start.month)
-	today = date.today()
-	today = (today.year, today.month)
-	while start <= today:
-		month_hist["{}-{:02d}".format(*start)] = 0
-		start = (start[0], start[1] + 1)
-		if start[1] > 12:
-			start = (start[0] + 1, 1)
+    month_hist = dict()
+    start = db.session.query(func.min(Entry.date_added)).filter(
+        Entry.group_id == group_id).one()[0]
+    start = (start.year, start.month)
+    today = date.today()
+    today = (today.year, today.month)
+    while start <= today:
+        month_hist["{}-{:02d}".format(*start)] = 0
+        start = (start[0], start[1] + 1)
+        if start[1] > 12:
+            start = (start[0] + 1, 1)
 
-	for e in g.entries:
-		month_hist[e.date_added.strftime("%Y-%m")] += 1
+    for e in g.entries:
+        month_hist[e.date_added.strftime("%Y-%m")] += 1
 
-	s['month_hist'] = (month_hist, max(month_hist.values()))
+    s['month_hist'] = (month_hist, max(month_hist.values()))
 
-	length_hist = dict()
-	total_len = 0
-	length_step = 200
-	for i in xrange(18):
-		length_hist[i * length_step] = 0
+    length_hist = dict()
+    total_len = 0
+    length_step = 200
+    for i in xrange(18):
+        length_hist[i * length_step] = 0
 
-	longest = dict()
-	for e in g.entries:
-		l = len(e.notes)
-		total_len += l
-		length_hist[int(l / length_step) * length_step] += 1
-		longest[e.title] = l
+    longest = dict()
+    for e in g.entries:
+        l = len(e.notes)
+        total_len += l
+        length_hist[int(l / length_step) * length_step] += 1
+        longest[e.title] = l
 
-	s['length_hist'] = (length_hist, max(length_hist.values()))
+    s['length_hist'] = (length_hist, max(length_hist.values()))
 
-	s['longest'] = sorted(longest.items(), key=operator.itemgetter(1), reverse=True)[:16]
-	s['shortest'] = sorted(longest.items(), key=operator.itemgetter(1))[:16]
-	s['total_len'] = total_len
-	s['avg_len'] = total_len / g.entries.count()
+    s['longest'] = sorted(
+        longest.items(), key=operator.itemgetter(1), reverse=True)[:16]
+    s['shortest'] = sorted(longest.items(), key=operator.itemgetter(1))[:16]
+    s['total_len'] = total_len
+    s['avg_len'] = total_len / g.entries.count()
 
-	return render_template('stats.html', group=g, stats=s)
+    return render_template('stats.html', group=g, stats=s)
 
 
 @app.route('/download')
 @login_required
 def download():
-	obj = {}
-	for g in Group.query.all():
-		obj[g.name] = {}
-		for e in g.entries.all():
-			obj[g.name][e.title] = {
-			'creator': e.creator,
-			'notes': e.notes,
-			'date': str(e.date_added)
-			}
+    obj = {}
+    for g in Group.query.all():
+        obj[g.name] = {}
+        for e in g.entries.all():
+            obj[g.name][e.title] = {
+                'creator': e.creator,
+                'notes': e.notes,
+                'date': str(e.date_added)
+            }
 
-	return Response(json.dumps(obj, indent=4, separators=(',', ': ')),
-					mimetype='text/json',
-					headers={'Content-Disposition': 'inline; filename="qbnotes.json"'})
+    return Response(json.dumps(obj, indent=4, separators=(',', ': ')),
+                    mimetype='text/json',
+                    headers={'Content-Disposition': 'inline; filename="qbnotes.json"'})
 
 
 @app.route('/upload', methods=['POST'])
 @login_required
 def upload():
-	if request.files['file']:
-		try:
-			doc = json.loads(request.files['file'].read())
-			for group_name in doc:
-				group = Group.query.filter(Group.name == group_name).first()
-				if not group:
-					group = Group(group_name)
-					db.session.add(group)
+    if request.files['file']:
+        try:
+            doc = json.loads(request.files['file'].read())
+            for group_name in doc:
+                group = Group.query.filter(Group.name == group_name).first()
+                if not group:
+                    group = Group(group_name)
+                    db.session.add(group)
 
-				for title in doc[group_name]:
-					obj = doc[group_name][title]
+                for title in doc[group_name]:
+                    obj = doc[group_name][title]
 
-					entry = Entry.query.filter(Entry.title == title, Entry.creator == obj['creator']).first()
-					if not entry:
-						entry = Entry(title, obj['creator'], obj['notes'], group)
-						db.session.add(entry)
+                    entry = Entry.query.filter(
+                        Entry.title == title, Entry.creator == obj['creator']).first()
+                    if not entry:
+                        entry = Entry(title, obj['creator'], obj[
+                                      'notes'], group)
+                        db.session.add(entry)
 
-					entry.creator = obj['creator']
-					entry.notes = obj['notes']
-					entry.date_added = datetime.strptime(obj['date'], '%Y-%m-%d').date()
+                    entry.creator = obj['creator']
+                    entry.notes = obj['notes']
+                    entry.date_added = datetime.strptime(
+                        obj['date'], '%Y-%m-%d').date()
 
-			db.session.commit()
-		except ValueError:
-			pass
+            db.session.commit()
+        except ValueError:
+            pass
 
-	return redirect(url_for('all_groups'))
+    return redirect(url_for('all_groups'))
 
 
 if __name__ == '__main__':
-	db.create_all()
-	app.run(host='0.0.0.0', debug=True, port=5001)
+    db.create_all()
+    app.run(host='0.0.0.0', debug=True, port=5001)
